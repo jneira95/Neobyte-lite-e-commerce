49f804f122c13c409fa735d6869369c2767d69d4
Jorge <jorgengarriga@gmail.com>
2020-12-01 22:42:15 +0100

    Add new route and fix app.modules

--- neobyte-computer/src/app/app-routing.module.ts
+++ neobyte-computer/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DetailProductComponent } from './modules/detail-product/detail-product.component';
+import { HeaderComponent } from './core/header/header.component';
 
-const routes: Routes = [{ path: 'detail', component: DetailProductComponent }];
+const routes: Routes = [
+	{ path: '', component: HeaderComponent },
+	{ path: 'detail', component: DetailProductComponent }
+	// { path: '', redirectTo: '/detail', pathMatch: 'full' }
+];
 
 @NgModule({
 	declarations: [],
--- neobyte-computer/src/app/app.module.ts
+++ neobyte-computer/src/app/app.module.ts
@@ -1,31 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
-import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
 import { DetailProductComponent } from './modules/detail-product/detail-product.component';
-import { ImagesGalleryComponent } from './modules/detail-product/images-gallery/images-gallery.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		DetailProductComponent,
-		ImagesGalleryComponent,
-		FooterComponent,
-		HeaderComponent
-	],
+	declarations: [AppComponent, DetailProductComponent, HeaderComponent],
 	imports: [
 		BrowserModule,
+		AppRoutingModule,
 		BrowserAnimationsModule,
-		MatIconModule,
-		MatDividerModule,
-		IvyCarouselModule,
-		AppRoutingModule
+		MatIconModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
