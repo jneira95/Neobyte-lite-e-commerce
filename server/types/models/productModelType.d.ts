889bc1aff3608ec11368ec62a184a84743993072
Jorge <jorgengarriga@gmail.com>
2020-12-01 02:20:52 +0100

    Create typescript interface for product model

---
+++ server/types/models/productModelType.d.ts
@@ -0,0 +1,15 @@
+interface IProduct {
+name: string;
+reference: string;
+// 'part-number'?: string;
+// ean?: number;
+// price: number;
+// stock?: number;
+// 'product-status': boolean;
+// images: Array<string>;
+// 'general-specs'?: Array<string>;
+// shortDescription: string;
+// generalDescription: string;
+}
+
+export default IProduct;
