71f18a30b80cee81f70ace23c00f42830f2f0578
Jorge <jorgengarriga@gmail.com>
2020-12-04 18:45:38 +0100

    Remove testing object

--- client/src/app/components/header/side-navbar/side-navbar.component.ts
+++ client/src/app/components/header/side-navbar/side-navbar.component.ts
@@ -1,5 +1,3 @@
-/* eslint-disable no-mixed-spaces-and-tabs */
-/* eslint-disable no-tabs */
 import { Component } from '@angular/core'
 
 @Component({
@@ -8,83 +6,35 @@ import { Component } from '@angular/core'
   styleUrls: ['./side-navbar.component.scss']
 })
 export class SideNavbarComponent {
-	panelOpenState = false;
+  panelOpenState = false;
 
-	categories: any[] = [
-	  {
-	    category: 'portátiles',
-	    route: 'portatil',
-	    icon: 'laptop_chromebook',
-	    categoriesExpand: ['portatiles', 'gaming', 'workStation']
-	  },
-	  {
-	    category: 'periféricos',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'smartphones',
-	    route: 'smartphones',
-	    icon: 'smartphone'
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  },
-	  {
-	    category: 'elemento prueba scroll',
-	    route: 'perifericos',
-	    icon: 'mouse',
-	    categoriesExpand: ['teclados', 'ratones', 'monitores']
-	  }
-	];
+  categories: any[] = [
+    {
+      category: 'portátiles',
+      route: 'portatil',
+      icon: 'laptop_chromebook',
+      categoriesExpand: ['portatiles', 'gaming', 'workStation']
+    },
+    {
+      category: 'periféricos',
+      route: 'perifericos',
+      icon: 'mouse',
+      categoriesExpand: ['teclados', 'ratones', 'monitores']
+    },
+    {
+      category: 'smartphones',
+      route: 'smartphones',
+      icon: 'smartphone'
+    }
+  ];
 
-	closeSidebar (): void {
-	  const closeBtn = document.getElementById('toogleSideBar')
-	  closeBtn.style.transform = 'translateX(-350px)'
-	}
+  closeSidebar (): void {
+    const closeBtn = document.getElementById('toogleSideBar')
+    closeBtn.style.transform = 'translateX(-350px)'
+  }
 
-	openSidebar (): void {
-	  const openBtn = document.getElementById('toogleSideBar')
-	  openBtn.style.transform = 'translateX(0)'
-	}
+  openSidebar (): void {
+    const openBtn = document.getElementById('toogleSideBar')
+    openBtn.style.transform = 'translateX(0)'
+  }
 }
