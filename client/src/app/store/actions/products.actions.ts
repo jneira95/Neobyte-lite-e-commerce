28cda8bf77872264de92622b571823824453b961
Jorge <jorgengarriga@gmail.com>
2020-12-04 18:43:59 +0100

    Files change cause by eslint

--- client/src/app/store/actions/products.actions.ts
+++ client/src/app/store/actions/products.actions.ts
@@ -1,11 +1,11 @@
-import { Action } from '@ngrx/store';
-import { ProductItem } from '../models/product-item-model';
-import actionTypes from './actionTypes';
+import { Action } from '@ngrx/store'
+import { ProductItem } from '../models/product-item-model'
+import actionTypes from './actionTypes'
 
 export class LoadProductItem implements Action {
 	readonly type = actionTypes.LOAD_PRODUCT_DETAIL;
 
-	constructor(public payload: ProductItem) {}
+	constructor (public payload: ProductItem) {}
 }
 
 export type ProductAction = LoadProductItem;
