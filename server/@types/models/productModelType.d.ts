79f1727aef8e8f9618d441aa597ee4d12709fdb1
Jorge <jorgengarriga@gmail.com>
2020-12-06 12:30:05 +0100

    Add custom property express

---
+++ server/@types/models/productModelType.d.ts
@@ -0,0 +1,20 @@
+import { Document } from 'mongoose';
+
+interface ProductModel {
+  name: string;
+  reference: string;
+  'part-number'?: string;
+  ean?: number;
+  price: number;
+  stock: number;
+  brand?: string;
+  'product-status'?: boolean;
+  images?: Array<string>;
+  'general-specs'?: Array<string>;
+  shortDescription?: string;
+  generalDescription?: Array;
+}
+
+interface IProduct extends ProductModel, Document {}
+
+export default IProduct;
