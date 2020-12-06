08ad647f203203de932003769306c26a6b41400b
Jorge <jorgengarriga@gmail.com>
2020-12-06 12:33:29 +0100

    Configure /user router index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,11 +3,14 @@ import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import ProductModel from './models/productModel';
+import UserModel from './models/userModel';
 import productsRouter from './routes/productRouter';
+import usersRouter from './routes/userRouter';
 import startServer from './server';
 
 const app = express();
 const productRouter = productsRouter(ProductModel);
+const userRouter = usersRouter(UserModel);
 
 app.use(cors());
 app.use(morgan('tiny'));
@@ -15,5 +18,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/product', productRouter);
+app.use('/user', userRouter);
 
 startServer(app);
