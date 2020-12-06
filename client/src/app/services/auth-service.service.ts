96e76981d138ec74d5e023b06332ce0293caba68
Jorge <jorgengarriga@gmail.com>
2020-12-06 23:04:45 +0100

    Create Auth Services1

--- client/src/app/services/auth-service.service.ts
+++ client/src/app/services/auth-service.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 import { Observable } from 'rxjs'
+import { IUser } from '../store/models/user.model'
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
   constructor (private http: HttpClient) { }
+
+  newUser: IUser = {
+    username: 'jorge',
+    email: 'jorgengarriga@gmail.com',
+    password: 'asdqwe123'
+  }
 
   private authEnpoint = 'http://localhost:5000'
   private login = '/login'
   private signup = '/signup'
-  private me = '/me'
+  private me = '/profile'
 
     httpOptions = {
       headers: new HttpHeaders({
@@ -19,7 +26,16 @@ export class AuthServiceService {
       })
     }
 
-    login (user: object): Observable<User> {
+    userLogin (user: any): Observable<IUser> {
+      const url = `${this.authEnpoint}${this.login}`
+      console.log(user)
+      console.log(this.http.post(url, user))
 
+      return this.http.post<any>(url, user)
+    }
+
+    userRegister (user: object): Observable<IUser> {
+      const url = `${this.authEnpoint}${this.signup}`
+      return this.http.post<IUser>(url, user)
     }
 }
