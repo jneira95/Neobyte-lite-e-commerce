4b7969b271cdb38bb25d4fc1c7f04d1451820d8b
Jorge <jorgengarriga@gmail.com>
2020-12-07 20:02:25 +0100

    Add login button routing and sidebar

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { UserLoginStateService } from '../../services/user-login-state.service'
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  constructor (private userLoginState: UserLoginStateService) {}
+  logged: boolean
+  ngOnInit (): void {
+    this.userLoginState.getUser() !== null ? this.logged = true : this.logged = false
+  }
+}
