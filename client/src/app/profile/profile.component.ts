a71289278b9142a952a107dd0de845d9aeea290d
Jorge <jorgengarriga@gmail.com>
2020-12-07 19:55:38 +0100

    Add profile component

---
+++ client/src/app/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core'
+import { UserLoginStateService } from '../services/user-login-state.service'
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+  constructor (private userLoginState: UserLoginStateService) {}
+  userData: object = {}
+  ngOnInit (): void {
+    this.userData = { ...JSON.parse(localStorage.getItem('user')) }
+  }
+
+  logout (): void {
+    this.userLoginState.removeUser()
+  }
+}
