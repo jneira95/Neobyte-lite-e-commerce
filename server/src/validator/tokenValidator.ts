7a300aec0d34e312f7f29f2677660a8407371b6f
Jorge <jorgengarriga@gmail.com>
2020-12-07 20:05:00 +0100

    Modified error message

--- server/src/validator/tokenValidator.ts
+++ server/src/validator/tokenValidator.ts
@@ -3,13 +3,13 @@ import { Request, Response, NextFunction } from 'express';
 
 export default function tokenValidation(req: Request, res: Response, next: NextFunction) {
   const token: any = req.header('token');
-  if (!token) return res.status(401).json({ message: 'AUTH_ERROR' });
+  if (!token) return res.status(401).json({ errors: [{ msg: 'AUTH_ERROR' }] });
   try {
     const decoded:any = jwt.verify(token, 'secret');
     req.body.user = decoded.id;
     next();
   } catch (error) {
-    res.status(500).send({ message: 'INVALID_TOKEN' });
+    res.status(500).json({ errors: [{ msg: 'INVALID_TOKEN' }] });
   }
   return true;
 }
