8284d8e5907597f83cf1571783669a664a215ed3
Jorge <jorgengarriga@gmail.com>
2020-12-07 20:04:09 +0100

    Add auth service

--- client/src/app/services/auth-service.service.ts
+++ client/src/app/services/auth-service.service.ts
@@ -1,41 +1,72 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { Router } from '@angular/router'
 import { Observable } from 'rxjs'
-import { IUser } from '../store/models/user.model'
+import { catchError, tap } from 'rxjs/operators'
+import { IUser, ILogin } from '../store/models/user.model'
+import { MessageService } from './error-message.service'
+import { UserLoginStateService } from './user-login-state.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor (private http: HttpClient) { }
+  constructor (
+    private router: Router,
+    private http: HttpClient,
+    private messageService: MessageService,
+    private userLoginState: UserLoginStateService
+  ) { }
 
-  newUser: IUser = {
-    username: 'jorge',
-    email: 'jorgengarriga@gmail.com',
-    password: 'asdqwe123'
-  }
-
-  private authEnpoint = 'http://localhost:5000'
+  private authEnpoint = 'http://localhost:5000/user'
   private login = '/login'
   private signup = '/signup'
-  private me = '/profile'
+  private profile = '/profile'
 
-    httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
+  log (message: string[]): void {
+    this.messageService.add(message)
+  }
+
+  validateToken (token: any): any {
+    const url = `${this.authEnpoint}${this.profile}`
+    return this.http.get<any>(url, { headers: { ...this.httpOptions, ...token } })
+      .subscribe((user) => {
+        this.userLoginState.setUser(user)
+        this.router.navigate(['/profile'])
       })
-    }
+  }
 
-    userLogin (user: any): Observable<IUser> {
-      const url = `${this.authEnpoint}${this.login}`
-      console.log(user)
-      console.log(this.http.post(url, user))
+  userLogin (user: any): Observable<ILogin> {
+    const url = `${this.authEnpoint}${this.login}`
+    return this.http.post<any>(url, user).pipe(
+      tap(token => this.validateToken(token)),
+      catchError(async (error) => {
+        console.log(error)
+        this.log(
+          error.error.errors
+            .map((error: any) => error.msg)
+        )
+      })
+    )
+  }
 
-      return this.http.post<any>(url, user)
-    }
-
-    userRegister (user: object): Observable<IUser> {
-      const url = `${this.authEnpoint}${this.signup}`
-      return this.http.post<IUser>(url, user)
-    }
+  userRegister (user: object): Observable<IUser> {
+    const url = `${this.authEnpoint}${this.signup}`
+    return this.http.post<any>(url, user).pipe(
+      tap(token => this.validateToken(token)),
+      catchError(async (error) => {
+        console.log(error)
+        this.log(
+          error.error.errors
+            .map((error: any) => error.msg)
+        )
+      })
+    )
+  }
 }
