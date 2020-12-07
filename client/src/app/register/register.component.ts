7fd38fd18e7ad0fc64a3bcf122bb64e97dffbb00
Jorge <jorgengarriga@gmail.com>
2020-12-07 20:03:36 +0100

    Add register component

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { Validators, FormBuilder } from '@angular/forms'
+import { AuthService } from '../services/auth-service.service'
+import { MessageService } from '../services/error-message.service'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
-  constructor () { }
+export class RegisterComponent {
+  constructor (
+    private authService: AuthService,
+    public messageService: MessageService,
+    private formBuilder: FormBuilder
+  ) {}
 
-  ngOnInit (): void {
-  }
+   pruebas: string[] = this.messageService.messages
+
+    registerForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+
+    onSubmit () {
+      this.authService.userRegister(this.registerForm.value).subscribe((event) => {
+        this.messageService.clear()
+        console.log('----> login component')
+      })
+    }
 }
