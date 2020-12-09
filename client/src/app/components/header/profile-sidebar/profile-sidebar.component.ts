01a31bdf794d27e8093b45624eff4c1e233874c5
Jorge <jorgengarriga@gmail.com>
2020-12-09 23:08:42 +0100

    Add header test

--- client/src/app/components/header/profile-sidebar/profile-sidebar.component.ts
+++ client/src/app/components/header/profile-sidebar/profile-sidebar.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-profile-sidebar',
   templateUrl: './profile-sidebar.component.html',
   styleUrls: ['./profile-sidebar.component.scss']
 })
-export class ProfileSidebarComponent implements OnInit {
-  constructor () { }
-
-  ngOnInit (): void {
-  }
-
+export class ProfileSidebarComponent {
   closeProfileSidebar (): void {
     const closeBtn = document.getElementById('toogleProfileSideBar')
     closeBtn.style.transform = 'translateX(350px)'
--- client/src/app/components/header/side-navbar/side-navbar.component.spec.ts
+++ client/src/app/components/header/side-navbar/side-navbar.component.spec.ts
@@ -1,24 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
 import { SideNavbarComponent } from './side-navbar.component'
+import { RouterModule } from '@angular/router'
+import { RouterTestingModule } from '@angular/router/testing'
 
 describe('SideNavbarComponent', () => {
-  // let component: SideNavbarComponent;
+  let component: SideNavbarComponent
   let fixture: ComponentFixture<SideNavbarComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterModule.forRoot([]), RouterTestingModule],
       declarations: [SideNavbarComponent]
     }).compileComponents()
   })
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SideNavbarComponent)
-    // component = fixture.componentInstance;
+    component = fixture.componentInstance
     fixture.detectChanges()
   })
 
   it('should create', () => {
-    expect(SideNavbarComponent).toBeTruthy()
+    expect(component).toBeTruthy()
+  })
+  it('should call open and close nav bar', () => {
+    component.openSidebar()
+    component.closeSidebar()
   })
 })
