313bcc5e0fec70080377f80fce599ad87a2b2db2
Jorge <jorgengarriga@gmail.com>
2020-12-10 20:54:54 +0100

    Add new controller for product list

--- server/src/controllers/products/productController.ts
+++ server/src/controllers/products/productController.ts
@@ -5,13 +5,26 @@ function productController(ProductModel: any) {
     try {
       const { id } = req.params;
       ProductModel.findById(id, (errorFindProduct: any, product: any) => {
+        if (errorFindProduct) res.send(errorFindProduct);
         res.json(product);
       });
     } catch (error) {
       res.send(error);
     }
   }
-  return { getProductById };
+
+  function getProductList(req: Request, res: Response) {
+    try {
+      const { query } = req;
+      ProductModel.find(query, (errorLoadingProducts: any, products: any) => {
+        if (errorLoadingProducts) res.send(errorLoadingProducts);
+        res.json(products);
+      });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+  return { getProductById, getProductList };
 }
 
 export default productController;
