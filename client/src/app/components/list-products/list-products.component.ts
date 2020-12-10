30a83c42fd2c752f8c25f1d7877fd27400985c09
Jorge <jorgengarriga@gmail.com>
2020-12-10 20:53:30 +0100

    Add list component connected with backend and detail

--- client/src/app/components/list-products/list-products.component.ts
+++ client/src/app/components/list-products/list-products.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core'
+import { Component } from '@angular/core'
+import { Observable } from 'rxjs'
+import { ActivatedRoute } from '@angular/router'
+import { ProductService } from '../../services/product.service'
+import { IProductItem } from '../../store/models/product-item-model'
 
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
   styleUrls: ['./list-products.component.scss']
 })
-export class ListProductsComponent implements OnInit {
-  constructor () { }
+export class ListProductsComponent {
+  constructor (
+     private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
 
   ngOnInit (): void {
   }
 
-  pruebas: string[] = ['pruebap ruebaprue baprueba prueba prueba prueba prueba', 'pruebas', 'pruebaspruebaspruebaspruebas', 'pruebas', 'pruebas', 'pruebas', 'pruebas']
+  // get paramId (): string {
+  // return this.route.snapshot.paramMap.get('id')
+  // }
+
+  productList$: Observable<IProductItem> = this.productService.getProductList();
 }
