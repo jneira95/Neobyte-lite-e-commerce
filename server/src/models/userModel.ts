9e80b7f3f48466f7c9a88fac3b4c175e7b2fb32e
Jorge <jorgengarriga@gmail.com>
2020-12-10 12:14:59 +0100

    Change folderName

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -1,5 +1,5 @@
 import { Schema, model } from 'mongoose';
-import IUser from '../../@types/models/userModelType';
+import IUser from '../../types/models/userModelType';
 
 const userSchema = new Schema({
   username: {
