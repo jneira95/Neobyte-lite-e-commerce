bcada148f324c99409b6315566f945ea1b898eb6
Jorge <jorgengarriga@gmail.com>
2020-12-13 13:38:09 +0100

    Add shoppingCartController to manage new cart and update cart

--- server/src/controllers/carts/shoppingCartController.ts
+++ server/src/controllers/carts/shoppingCartController.ts
@@ -1,11 +1,41 @@
 import { Request, Response } from 'express';
 
-function shoppingCartController(ShoppingCartModel: any) {
-  function getCart(req: Request, res: Response) {
-    ShoppingCartModel.find({});
-    return res;
-  }
-  return { getCart };
+function shoppingCartController(ShoppingCartModel: any, UserModel: any) {
+  const assignUserShoppingCart = async (userId: string, shoppingCartId: string) => {
+    const query = { _id: userId };
+    const update = { shoppingCart: shoppingCartId };
+    await UserModel.findOneAndUpdate(query, update);
+  };
+
+  const updateUserShoppingCart = async (shoppingCartId: string, update: object) => {
+    const query = { _id: shoppingCartId };
+    const updatedCart = await ShoppingCartModel.findOneAndUpdate(query, update, { new: true });
+    return updatedCart;
+  };
+
+  const userShoppingCart = async (req: Request, res: Response): Promise<any> => {
+    const { _id, ...cart } = req.body;
+
+    const isUserExists = await UserModel.exists({ _id });
+    if (!isUserExists) return res.status(400).json({ errors: [{ msg: 'Debe iniciar sesión para añadir articulos al carrito' }] });
+
+    const checkUserShoppingCart = await UserModel.findById(_id);
+    if (checkUserShoppingCart.shoppingCart !== undefined) {
+      const updatedCart = await updateUserShoppingCart(checkUserShoppingCart.shoppingCart, cart);
+      return res.json(updatedCart);
+    }
+
+    const shoppingCart = new ShoppingCartModel({
+      ...cart,
+    });
+
+    await assignUserShoppingCart(_id, shoppingCart._id);
+    const newShoppingCart = await ShoppingCartModel.create(shoppingCart);
+    res.json(newShoppingCart);
+    return true;
+  };
+
+  return { userShoppingCart };
 }
 
 export default shoppingCartController;
