27732b5fa781962847a2b04ed41af4302b6a453b
Jorge <jorgengarriga@gmail.com>
2020-12-22 13:40:30 +0100

    Update startServer.ts

--- server/src/startServer.ts
+++ server/src/startServer.ts
@@ -6,7 +6,7 @@ import { connect } from 'mongoose';
 const startServer = (app: Express) => {
   try {
     const port = process.env.PORT || 5000;
-    const dbUrl = process.env.DBURL || 'mongodb+srv://jneira95:5o7harhz@cluster0.hykgh.mongodb.net/neobytecomputerDB?retryWrites=true&w=majority';
+    const dbUrl = process.env.DBURL || 'mongodb+srv://jneira95:*******@cluster0.hykgh.mongodb.net/neobytecomputerDB?retryWrites=true&w=majority';
     connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
     const server = http.createServer(app);
     const debug = Debug('app');
